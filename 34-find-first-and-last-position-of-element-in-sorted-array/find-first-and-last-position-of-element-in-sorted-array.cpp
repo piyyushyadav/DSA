e911f1c38102629638a785e9f82ec29200ea5f1d
Piyush yadav <164665354+piyyushyadav@users.noreply.github.com>
2026-01-27 19:28:11 +0530

    Time: 0 ms (100.00%) | Memory: 18.4 MB (17.77%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,21 +1,75 @@
 class Solution {
 public:
+  int leftele(vector<int> nums,int target){
+
+    int n  = nums.size();
+
+    int l =0;
+    int r = n-1;
+    int le  =-1;
+
+    while(l<=r){
+        int mid=  l +(r-l)/2;
+
+        if(nums[mid] == target){
+            le =mid;
+            r = mid-1;
+        }
+        else if(target>nums[mid]){
+            l = mid+1;
+        }
+        else{
+            r = mid-1;
+        }
+    }
+    return le;
+
+  }
+
+    int rightele(vector<int> nums,int target){
+
+    int n  = nums.size();
+
+    int l =0;
+    int r = n-1;
+    int re  =-1;
+
+    while(l<=r){
+        int mid=  l +(r-l)/2;
+
+        if(nums[mid] == target){
+            l = mid+1;
+            re = mid;
+        }
+        else if(target>nums[mid]){
+            l = mid+1;
+        }
+        else{
+            r = mid-1;
+        }
+    }
+
+    return re;
+
+
+
+
+
+
+  }
+
+
     vector<int> searchRange(vector<int>& nums, int target) {
-        if (nums.size() == 0) return {-1, -1};
-        if (nums.size() == 1) {
-            if (nums[0] == target) return {0, 0};
-            else return {-1, -1};
-        }
+        vector<int>ans;
 
-        vector<int> result(2, -1);  // default [-1, -1]
+        int left = leftele(nums,target);
 
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] == target) {
-                if (result[0] == -1) result[0] = i;  // First occurrence
-                result[1] = i;                       // Last occurrence
-            }
-        }
+        int right = rightele(nums,target);
 
-        return result;
+
+        ans.push_back(left);
+        ans.push_back(right);
+
+        return ans;
     }
 };
