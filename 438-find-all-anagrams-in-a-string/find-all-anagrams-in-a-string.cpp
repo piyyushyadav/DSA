51839b1d2b9c8903ca4855efadba558f8b00446d
Piyush yadav <164665354+piyyushyadav@users.noreply.github.com>
2026-01-30 14:56:46 +0530

    Time: 36 ms (10.69%) | Memory: 36.8 MB (6.64%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
@@ -1,53 +1,60 @@
 class Solution {
 public:
+  bool allzero(vector<int>  nums){
+    for(int x:nums){
+        if(x!=0){
+            return false;
+        }
+        
+    
+    }
 
-bool allzero(vector<int>&arr){
-      int n = arr.size();
-      
-      for(int  i = 0 ; i<n;i++){
-          if(arr[i]!=0){
-              return false;
-          }
-      }
-      return true;
+    return true;
   }
-
     vector<int> findAnagrams(string s, string p) {
-         vector<int>counter(26,0);
-         
-         for(int i = 0 ; i<p.length() ; i ++){
-            char ch = p[i];
-            counter[ch - 'a']++;
+        vector<int> counter(26,0);
+        int n = p.size();
+        for(int  i = 0 ;i<n;i++){
+            counter[p[i] - 'a']++;
+        }
+
+        vector<int> ans;
+        int i = 0 ;
+        int  j =0;
+       
+        int  k = s.size();
+
+        while(j<k){
+            counter[s[j]-'a']--;
+
+            if(j-i+1 ==n){
+                if(allzero(counter)){
+                    ans.push_back(i);
+                }
+                counter[s[i] -'a']++;
+                i++;
 
 
-
-
-         }
-
-         int  i = 0 ;
-          int j =  0;
-
-          
-          int n  = s.size();
-          vector<int> ans;
-          int k = p.size();
-          while(j<n){
-            counter[s[j] - 'a'] --;
-            
-            
-            if(j-i+1==k){
-            if(allzero(counter)){
-                ans.push_back(i);
-            }
-            
-           counter[s[i] - 'a']++;
-            i++;
             }
             j++;
-            
-        
 
-          }
-          return ans;
+
+        }
+        return ans;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+         
     }
 };
