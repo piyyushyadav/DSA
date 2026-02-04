d7f9a8051b3af87eadcf3f8cbba29f1917f358ba
Piyush yadav <164665354+piyyushyadav@users.noreply.github.com>
2026-02-04 23:41:10 +0530

    Time: 12 ms (91.11%) | Memory: 107.3 MB (40.15%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.cpp
+++ 739-daily-temperatures/daily-temperatures.cpp
@@ -1,27 +1,30 @@
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
+        //yeha greater element nikalna hai toh monotoi=nic decrising stack 
+        // right side mai toh 0 se n tak
+
+
         stack<int> st;
         
         int n = temperatures.size();
         vector<int> ans(n,0);
-
-        for(int i = 0 ;i<n;i++){
-            int count = 0;
-            while(!st.empty() && temperatures[st.top()]<temperatures[i]){
-               int prev = st.top();
+        for(int i =0;i<n;i++){
+            int count =0;
+        
+          while(!st.empty() && temperatures[st.top()]<temperatures[i]){
+           // yeha pe prev mai index store kiya hai usko i se minus krke days count kr liya hai
+           
+            int prev = st.top();
 
                 st.pop();
 
-                ans[prev] = i - prev;
-                
+                ans[prev] = i - prev; 
+
             }
             
             st.push(i);
-
         }
-
         return ans;
-        
     }
 };
