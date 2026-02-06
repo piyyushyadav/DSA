24f195e0bb4fa059015180f5880ab8775e2b53a6
Piyush yadav <164665354+piyyushyadav@users.noreply.github.com>
2026-02-06 19:59:33 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (53.28%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,62 +1,60 @@
 class Solution {
 public:
+     int leftocc(vector<int> &nums,int target){
+        int n = nums.size();
+        int l =0;
+        int r = n-1;
+        int left =-1;
 
-int leftmost(vector<int> &nums,int target){
-    int left = -1;
-    int n = nums.size();
-    int l = 0;
-    int r = n-1;
-    while(l<=r){
-        int mid = l +(r-l)/2;
+        while(l<=r){
+           int mid = l+ (r-l)/2;
+            if(nums[mid] == target){
+                left = mid;
+                r = mid-1;
+                 }
 
-        if(nums[mid] == target){
-            left = mid;
-            r = mid-1;
+                else if(nums[mid]>target){
+                    r = mid-1;
+                }
+                else{
+                    l = mid+1;
+                }
         }
-        else if(nums[mid]>target){
-            r = mid-1;
-        }
-        else{
-            l = mid +1;
-        }
-    }
+        return left;
 
-    return left;
-}
+     }
+     int rightocc(vector<int> &nums,int target){
+        int n = nums.size();
+        int l =0;
+        int r = n-1;
+        int right =-1;
 
-int rightmost(vector<int> &nums,int target){
-    int right = -1;
-    int n = nums.size();
-    int l = 0;
-    int r = n-1;
-    while(l<=r){
-        int mid = l +(r-l)/2;
+        while(l<=r){
+           int mid = l+ (r-l)/2;
+            if(nums[mid] == target){
+                right = mid;
+                l = mid+1;
+                 }
 
-        if(nums[mid] == target){
-            right = mid;
-            l = mid+1;
+                else if(nums[mid]>target){
+                    r = mid-1;
+                }
+                else{
+                    l = mid+1;
+                }
         }
-        else if(nums[mid]>target){
-            r = mid-1;
-        }
-        else{
-            l = mid +1;
-        }
-    }
+        return right;
 
-    return right;
-}
+     }
+
+
+
     vector<int> searchRange(vector<int>& nums, int target) {
-        //trying the binary approah
-
-       int left_most = leftmost(nums,target);
-       int right_most = rightmost(nums,target);
-
-       return{left_most,right_most};
-
-        
+     int left = leftocc(nums,target);
+     int right = rightocc(nums,target);
 
 
+     return {left,right};
 
     }
 };
