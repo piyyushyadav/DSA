e0896312fbb3c153715b180054435b68a1021c6d
Piyush yadav <164665354+piyyushyadav@users.noreply.github.com>
2026-02-06 15:23:39 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (18.58%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,26 +1,60 @@
 class Solution {
 public:
+
+int leftmost(vector<int> &nums,int target){
+    int left = -1;
+    int n = nums.size();
+    int l = 0;
+    int r = n-1;
+    while(l<=r){
+        int mid = l +(r-l)/2;
+
+        if(nums[mid] == target){
+            left = mid;
+            r = mid-1;
+        }
+        else if(nums[mid]>target){
+            r = mid-1;
+        }
+        else{
+            l = mid +1;
+        }
+    }
+
+    return left;
+}
+
+int rightmost(vector<int> &nums,int target){
+    int right = -1;
+    int n = nums.size();
+    int l = 0;
+    int r = n-1;
+    while(l<=r){
+        int mid = l +(r-l)/2;
+
+        if(nums[mid] == target){
+            right = mid;
+            l = mid+1;
+        }
+        else if(nums[mid]>target){
+            r = mid-1;
+        }
+        else{
+            l = mid +1;
+        }
+    }
+
+    return right;
+}
     vector<int> searchRange(vector<int>& nums, int target) {
-        int n = nums.size();
-        vector<int> ans(2,-1);
+        //trying the binary approah
 
-        for(int i =0 ;i<n;i++){
-            if(nums[i] == target){
-              ans[0] = i;
-                break;
-            }
+       int left_most = leftmost(nums,target);
+       int right_most = rightmost(nums,target);
 
-        }
-         for(int i =n-1 ;i>=0;i--){
+       return{left_most,right_most};
 
-            if(nums[i] == target){
-                ans[1] =i;
-                break;
-            }
-
-        }
-
-        return ans;
+        
 
 
 
