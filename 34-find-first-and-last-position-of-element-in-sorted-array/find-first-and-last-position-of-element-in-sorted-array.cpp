2af0ad2bb2f4bb725dee5c70e6d6539d7d30f8e8
Piyush yadav <164665354+piyyushyadav@users.noreply.github.com>
2026-02-06 14:57:36 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (18.58%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,75 +1,28 @@
 class Solution {
 public:
-  int leftele(vector<int> nums,int target){
-
-    int n  = nums.size();
-
-    int l =0;
-    int r = n-1;
-    int le  =-1;
-
-    while(l<=r){
-        int mid=  l +(r-l)/2;
-
-        if(nums[mid] == target){
-            le =mid;
-            r = mid-1;
-        }
-        else if(target>nums[mid]){
-            l = mid+1;
-        }
-        else{
-            r = mid-1;
-        }
-    }
-    return le;
-
-  }
-
-    int rightele(vector<int> nums,int target){
-
-    int n  = nums.size();
-
-    int l =0;
-    int r = n-1;
-    int re  =-1;
-
-    while(l<=r){
-        int mid=  l +(r-l)/2;
-
-        if(nums[mid] == target){
-            l = mid+1;
-            re = mid;
-        }
-        else if(target>nums[mid]){
-            l = mid+1;
-        }
-        else{
-            r = mid-1;
-        }
-    }
-
-    return re;
-
-
-
-
-
-
-  }
-
-
     vector<int> searchRange(vector<int>& nums, int target) {
-        vector<int>ans;
+        int n = nums.size();
+        vector<int> ans(2,-1);
 
-        int left = leftele(nums,target);
+        for(int i =0 ;i<n;i++){
+            if(nums[i] == target){
+              ans[0] = i;
+                break;
+            }
 
-        int right = rightele(nums,target);
+        }
+         for(int i =n-1 ;i>=0;i--){
 
+            if(nums[i] == target){
+                ans[1] =i;
+                break;
+            }
 
-        ans.push_back(left);
-        ans.push_back(right);
+        }
 
         return ans;
+
+
+
     }
 };
