4514aa7ed7271e8262390d369ef4eebe5178323a
Piyush yadav <164665354+piyyushyadav@users.noreply.github.com>
2026-02-09 21:00:09 +0530

    Time: 0 ms (100.00%) | Memory: 13.7 MB (42.06%) - LeetSync

--- 35-search-insert-position/search-insert-position.cpp
+++ 35-search-insert-position/search-insert-position.cpp
@@ -1,15 +1,33 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        int  n = nums.size();
-        int ans; 
-        for(int i =0;i<n;i++){
-            if(nums[i]>= target){
-                ans = i;
-                break;
-            }
+        
+        // binary search approach for o(logn);
+
+        int n = nums.size();
+        int  l =0;
+        int r = n-1;
+        int ans = n;
+
+        while(l<=r){
+            int  mid = l + (r-l)/2;
+
             
+             if(nums[mid]>=target ){
+                ans = mid;
+
+              r =mid-1;
+                
+
+                
+            }
+            else{
+                l= mid+1;
+            }
         }
+
         return ans;
+
+        
     }
 };
