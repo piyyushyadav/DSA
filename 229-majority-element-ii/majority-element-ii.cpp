4beca363595f4e292d153fa2add62ab3b8450df0
Piyush yadav <164665354+piyyushyadav@users.noreply.github.com>
2026-03-13 10:04:46 +0530

    Time: 0 ms (100.00%) | Memory: 23.6 MB (64.41%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,80 +1,57 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int count1 =0 ;
-
-        int ele1 =  0;
-
-
-        int count2 =0 ;
-
-        int ele2 = 1;
-
-
-
-        for(int i =0 ;i<nums.size();i++){
-
-
-            if(nums[i] == ele1){
-                count1 ++;
-            }
-           else if(nums[i] == ele2){
-            count2++;
-           }
-
-
-           else if(count1 ==0){
-
-            ele1  = nums[i];
-            count1++;
-           }
-
-           else if(count2 == 0){
-            ele2 = nums[i];
-            count2++;
-           }
-
-           else{
-            count1--;
-            count2--;
-           }
-        }
-
-
-        int fre1 = 0;
-
-        int fre2 = 0;
-
-
-        vector<int> ans;
-
         int n = nums.size();
-        for(int  i = 0 ;i<nums.size();i++){
-            if(nums[i] == ele1){
-                fre1++;
-                
+        int maj1 = 0;
+        int count1 = 0;
+        int maj2 = 0;
+        int count2 =0;
+
+
+        for(int i =0;i<n;i++){
+           
+           if(nums[i] == maj1){
+                count1++;
+            }
+            else if(nums[i] == maj2){
+                count2++;
             }
 
-
-         else if(nums[i] == ele2){
-            fre2++;
-            
-         }
+            else if(count1 ==0){
+                maj1 = nums[i];
+                count1++;
+            }
+            else if(count2 == 0 ){
+                maj2 = nums[i];
+                count2++;
+            }
+            else{
+                count1--;
+                count2--;
+            }
         }
 
-          if(fre1>n/3){
-            ans.push_back(ele1);
-         }
-
-         if(fre2>n/3){
-            ans.push_back(ele2);
-         }
-         return ans;
-
+        vector<int> result;
+        int fre1 =0;
+        int fre2 = 0;
+        for(int n : nums){
+            if(n == maj1){
+                fre1++;
+            }
+            else if(n == maj2){
+                fre2++;
+            }
+        }
+        if(fre1>floor(n/3)){
+            result.push_back(maj1);
+        }
+        if(fre2>floor(n/3)){
+            result.push_back(maj2);
         }
 
-        
+
+        return result;
 
 
-    
+    }
 };
