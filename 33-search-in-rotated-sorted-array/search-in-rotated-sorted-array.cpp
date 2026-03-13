424d6bf69bf83ad53b39aa6c12d9a67cddb584f6
Piyush yadav <164665354+piyyushyadav@users.noreply.github.com>
2026-03-13 13:04:36 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (34.73%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,56 +1,59 @@
 class Solution {
-public: 
-int pivitele(vector<int>&nums){
-    int n = nums.size();
-    int l =0;   
-    int r = n-1;
+public:
 
-    while(l<r){
-        int mid = l+ (r-l)/2;
-        if(nums[mid]>nums[r]){
-            l = mid +1;
-        }
-        else{
-            r = mid;
-        }
+int solvepivit(vector<int> &nums){
+    int  n = nums.size();
+  int  l = 0;
+  int r  = n-1;
+
+  while(l<r){
+    int mid =  l +(r-l)/2;
+
+    if(nums[mid]>nums[r]){
+        l = mid+1;
     }
+    else{
+        r = mid;
+        
+    }
+    
 
-    return r;
+
+  }
+  return r;
 }
 
-int binary(int l,int r, vector<int>&nums,int target){
-    int n = nums.size();
+int binarys(int l,int r,vector<int> &nums,int target){
+    int  n = nums.size();
+    int index = -1;
 
     while(l<=r){
-        int mid = l + (r-l)/2;
-
+        int mid = l +(r-l)/2;
         if(nums[mid] == target){
-            return mid;
+            return  mid;
         }
         else if(nums[mid]<target){
             l = mid+1;
-
         }
         else{
             r = mid-1;
         }
+
     }
-    return -1;
+
+    return index;
 }
     int search(vector<int>& nums, int target) {
-        int n  = nums.size();
+        int n = nums.size();
+        int pivit =  solvepivit(nums);
 
-        int pivit = pivitele(nums);
-        int a  = -1;
+       int ans = binarys(0,pivit-1,nums,target);
+       if(ans != -1){
+        return ans;
+       }
+       ans = binarys(pivit,n-1,nums,target);
 
-        a = binary(0,pivit-1,nums,target);
-        if(a != -1){
-            return a;
-        }
-        else{
-            return binary(pivit,n-1,nums,target);
-        }
-
-        
+       return ans;
+       
     }
 };
