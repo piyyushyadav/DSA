defc5db01f0f53a7ba4656f96729be94f7b0de1b
Piyush yadav <164665354+piyyushyadav@users.noreply.github.com>
2026-03-29 00:28:04 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (65.09%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,44 +1,27 @@
 class Solution {
 public:
-bool isthere(string a){
-    int n = a.size();
-    for(int i =0;i<n;i++){
-        for(int j=i+1;j<n;j++){
-            if(a[i]==a[j]){
-                return false;
-            }
-        }
-    }
-    return true;
-}
     int lengthOfLongestSubstring(string s) {
         int n = s.size();
+        int currcount = 0;
+        int maxcount = 0;
 
-
-
-        int i =0;
-        int j =0;
-        
-        string ans ="";
-        int count =0;
-        int max1 =0;
-
+       int i =0;
+        int j  =  0;
+        unordered_map<int,int> mp;
         while(j<n){
-            ans.push_back(s[j]);
-            count++;
-           
-            while(!isthere(ans)){
+
+            char ch = s[j];
+            mp[ch]++;
+            while(mp[s[j]]>1){
                 
-                ans.erase(0, 1);
+                mp[s[i]]--;
                 i++;
-                count--;
-               
-                
             }
-            max1 = max(max1, count);
+
+            maxcount = max(maxcount,j-i+1);
             j++;
-            
+
         }
-        return max1;
+        return maxcount;
     }
 };
