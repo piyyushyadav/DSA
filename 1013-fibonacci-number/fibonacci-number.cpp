e296e30e9e3a5bf5210c356780855d3555501cab
Piyush yadav <164665354+piyyushyadav@users.noreply.github.com>
2026-06-28 23:08:34 +0530

    Time: 14 ms (22.74%) | Memory: 7.9 MB (37.62%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int fib(int n) {
+        if(n==1){
+            return 1;
+
+        }
+
+        else if(n==0){
+            return 0;
+        }
+
+        int ans = fib(n-1)+fib(n-2);
+        return ans;
+    }
+};
