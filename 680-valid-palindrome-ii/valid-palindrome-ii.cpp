66753246a195d84fdc204947100b5e396f4e454f
Piyush yadav <164665354+piyyushyadav@users.noreply.github.com>
2026-07-02 14:46:05 +0530

    Time: 4 ms (58.69%) | Memory: 22.1 MB (79.01%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.cpp
+++ 680-valid-palindrome-ii/valid-palindrome-ii.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     bool isPalindrome(string &s, int i, int j){
-        while(i < j){
+        while(i<j){
             if(s[i] != s[j]){
                 return false;
             }
             i++;
             j--;
+
         }
+
         return true;
     }
 
