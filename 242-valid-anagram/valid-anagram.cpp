33dda9aa62ee83035e05ae370cb1a2327ec37876
Piyush yadav <164665354+piyyushyadav@users.noreply.github.com>
2026-07-03 14:48:28 +0530

    Time: 7 ms (19.17%) | Memory: 9.6 MB (75.96%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -4,6 +4,11 @@ public:
         sort(s.begin(),s.end());
         sort(t.begin(),t.end());
 
-        return s==t;
+        if(s==t){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 };
